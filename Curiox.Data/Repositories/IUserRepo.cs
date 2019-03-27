578a736f606a2d8d849f0b5fa36ff6f8b361caa8
toanpn190 <toanpn190@gmail.com>
2019-03-27 14:48:56 +0700

    Initial commit

---
+++ Curiox.Data/Repositories/IUserRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curiox.Data.Repositories
+{
+    public interface IUserRepo
+    {
+
+    }
+}
---
+++ Curiox.Data/Repositories/UserRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curiox.Data.Repositories
+{
+    class UserRepo
+    {
+    }
+}
