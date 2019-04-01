8c4742f9b74e95135bd48b5472dd2dbacd6bd99a
Lam Nguyen <lamnt437@gmail.com>
2019-04-01 17:11:32 +0700

    add TestController

---
+++ Curiox.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curiox.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Question()
+        {
+            return View();
+        }
+    }
+}
