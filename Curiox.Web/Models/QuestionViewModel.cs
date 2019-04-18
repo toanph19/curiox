67bff16b8402b113fbf3005adbcf03d0df1de755
Nguyen Van Huy <huynguyen290598@gmail.com>
2019-04-18 09:18:09 +0700

    I dont know how to switch to my branch so... huynv

---
+++ Curiox.Web/Models/QuestionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Curiox.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curiox.Web.Models
+{
+    public class QuestionViewModel : BaseEntity
+    {
+        public QuestionViewModel()
+        {
+            Answer = new HashSet<AnswerViewModel>();
+        }
+
+        public string Title { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string UserName { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<AnswerViewModel> Answer { get; set; }
+    }
+}
