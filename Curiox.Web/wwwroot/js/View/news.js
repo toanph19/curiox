adb9f1e522521629c6ffb9305ce37ef7f8d4d36f
Thanh NC <thanhnc138@gmail.com>
2019-05-05 16:33:24 +0700

    update question box and navbar user-box

---
+++ Curiox.Web/wwwroot/js/View/news.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    //handle event submit question
+    $('#btnSubmitQuestion').on('click', function () {
+        alert('submit');
+    });
+
+    //handle event click btn-answer
+    $('.btn-answer').on('click', function () {
+        alert('answer');
+    });
+
+    //handle event click btn-upvote
+    $('.btn-upvote').on('click', function () {
+        alert('upvote');
+    });
+
+    //handle event click btn-downvote
+    $('.btn-downvote').on('click', function () {
+        alert('downvote');
+    });
+});
---
+++ Curiox.Web/wwwroot/js/common/dialog.js
@@ -0,0 +1,29 @@
+﻿$('#question-box').dialog({
+    autoOpen: false,
+    title: "Add Question",
+    modal: true,
+    resizable: false,
+    width: 620,
+    height: 210,
+    beforeClose: function (event) {
+        $('.selector_input').val('');
+    },
+    buttons: [
+        {
+            text: "Add Question",
+            id: 'btnSubmitQuestion',
+            class: 'btn btn-primary',
+            click: function () {
+
+            }
+        },
+        {
+            text: "Cancel",
+            id: 'btnCancel',
+            class: 'btn',
+            click: function () {
+                $(this).dialog("close");
+            }
+        }
+    ]
+});
