36850f3ff525db163742b0affe2abc663056f8ba
Lam Nguyen <lamnt437@gmail.com>
2019-06-01 14:05:42 +0700

    partially done Upvote for answer

---
+++ Curiox.Web/wwwroot/js/View/question.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    //handle event click btn-upvote
+    $('.btn-upvote').on('click', function () {
+        let questionId = $(this).parents('.news-main-item').find('.question-title').attr('question-id');
+        var jsonObj = {
+            "token": localStorage.getItem('token'),
+        };
+        let thisUpvote = $(this).find('.nbUpvote');
+        $.ajax({
+            method: "POST",
+            url: Curiox.Config.loginUrl + "/Api/Question/Upvote?questionId=" + questionId,
+            contentType: "application/json",
+            data: JSON.stringify(jsonObj),
+            success: function (data, txtStatus, xhr) {
+                if (txtStatus === "success") {
+                    let nbUpvote = parseInt(thisUpvote.html());
+                    thisUpvote.html(++nbUpvote);
+                }
+            },
+            error: function () {
+                CommonJS.showFailMsg("An error occured! Please try again!");
+            }
+        });
+    });
+});
